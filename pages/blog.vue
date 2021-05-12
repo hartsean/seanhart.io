6f082c9fdaf747acb8df5fdb2b758a891d3b9a9a
hartsean <hartsean@gmail.com>
2021-05-12 16:55:38 -0500

    (feat) blog page

---
+++ pages/blog.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    blogggg
+    <!-- <a href="https://affectionate-aryabhata-d2b1f5.netlify.app/">🚬</a> -->
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+.container {
+  background: #fef9e9;
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+a.b-g:hover{
+  color: #DA2c38;
+  background: fixed;
+}
+a {
+  color: #016FB9;
+}
+a:hover{
+  color: #DA2c38;
+}
+.title {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  display: block;
+  color: #000;
+  min-height: 100%;
+  font-size: 2.1em;
+}
+
+.subtitle {
+  color: #526488;
+  padding-bottom: 15px;
+}
+
+</style>
